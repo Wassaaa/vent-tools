d807da59c650146d4d1af9354ac539039358b1d5
Al <allarklein@gmail.com>
2021-11-16 23:45:43 +0200

    merge button

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,4 +1,5 @@
-import { VentPart } from './../../VentPart';
+import { TimesService } from './../../services/times.service';
+import { VentPart } from 'src/app/VentPart';
 import { TableService } from './../../services/table.service';
 import { DataService } from '../../services/data.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
@@ -17,7 +18,8 @@ export class TableComponent implements OnInit {
 
   constructor(
     private dataService: DataService,
-    private tableService: TableService
+    private tableService: TableService,
+    private timesService: TimesService
   ) {}
 
   displayedColumns: string[] = ['sizeString', 'type', 'amount', 'timeString'];
@@ -39,6 +41,67 @@ export class TableComponent implements OnInit {
     }
     this.myDataSource = loadedData;
   }
+
+  onMerge() {
+    let newResult: VentPart[] = []; //variable for the new merged array
+
+    interface merge {
+      //interface for my object with info needed for my merging.
+      index: number;
+      amount: number;
+    }
+    let seen = new Map<string, merge>(); //map for setting up found elements that I plan to merge
+
+    const result = this.myDataSource.filter((element) => {
+      let mySpecialKey: string = element.type + element.sizeString; //special key for my Map that consists of 2 strings added tougether that i Use for main filtering
+
+      if (!seen.has(mySpecialKey)) {
+        //if my map doesn't have current element geting iterated then set it up in my Map with the special key and merge Object,
+        seen.set(mySpecialKey, {
+          //index so I can find the correct array element to add amount to and amount to add onto later.
+          index: newResult.length,
+          amount: element.amount,
+        });
+        //console.log(`${element.timeUsed} / ${element.amount}`);
+        element.timeUsed = element.timeUsed / element.amount;
+        newResult.push(element);
+        return true;
+      }
+      let myMerge = seen.get(element.type + element.sizeString);
+      if (myMerge) {
+        seen.set(mySpecialKey, {
+          index: myMerge.index,
+          amount: myMerge.amount + element.amount,
+        });
+        console.log(`${myMerge.amount} + ${element.amount}`);
+        if (seen.get(mySpecialKey)) {
+          let newAmount = seen.get(mySpecialKey)!.amount;
+          console.log('new amount from seen: ' + newAmount);
+          console.log(
+            'new amount from array: ' + newResult[myMerge.index].amount
+          );
+          newResult[myMerge.index].amount = newAmount;
+          //arr.splice(myMerge.index, 1);
+        }
+      }
+      return false;
+    });
+    newResult.map((x) => {
+      x.timeUsed = x.amount * x.timeUsed;
+      x.timeString = this.timesService.calculation(x.timeUsed);
+      return x;
+    });
+    // .map((v, i, a) => {
+    //   console.log(v.timeUsed);
+    //   v.timeString = this.timesService.calculation(v.timeUsed);
+    //   return v;
+    // });
+    console.log(seen.values());
+    console.log(result);
+    this.myDataSource = newResult;
+    this.table.renderRows();
+  }
+
   calculation() {
     let sum: number = 0;
     if (this.myDataSource)
